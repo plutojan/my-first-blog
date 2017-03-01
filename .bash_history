c18239b0cec13105ed5d188ca4a6ec4c22eb9f40
plutojan <plutojan@gmail.com>
2017-03-01 19:52:06 +0000

    My Django Girls app, first commit

---
+++ .bash_history
@@ -0,0 +1,2 @@
+http://localhost:8000/polls/
+mkvirtualenv django17 --python=/usr/bin/python3.4
---
+++ .virtualenvs/django17/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/plutojan/.virtualenvs/django17/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
